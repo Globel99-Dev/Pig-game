f53219aab7ba5f19cc840b02d067a5b6fc1e1681
Globel99-Dev <77055817+Globel99-Dev@users.noreply.github.com>
2021-03-21 23:32:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,24 +6,34 @@ const score = document.querySelectorAll('.score');
 const currentScore = document.querySelectorAll('.current-score');
 const dicePng = document.querySelector('.dice');
 const activePlayer = document.querySelector('.player--active');
+let scorePlayer1 = document.querySelector('.player--active').children[1];
 const activeScore = document.querySelector('.player--active').children[2]
   .children[1];
 
 const randomNumber = function () {
   const luckyNumber = Math.floor(Math.random() * 6) + 1;
+  dicePng.classList.remove('hidden');
+  //activeScore.textContent = `${luckyNumber}`;
+  let x = luckyNumber;
+  let y = Number(activeScore.textContent);
+  if (luckyNumber !== 1) activeScore.textContent = `${y + x}`;
+  /* for (let i = 0; i < x; i++) {
+    if (luckyNumber !== 1) {
+    }
+  }
   if (activeScore.textContent === '0') {
-    activeScore.textContent = `${luckyNumber}`;
   } else {
   }
-  /* console.log(`numerosumado = a luckynumber ${numeroSumado}`);
+  console.log(`numerosumado = a luckynumber ${numeroSumado}`);
   numeroSumado = numeroSumado + luckyNumber;
   console.log(`numerosumado + luckynumber = ${numeroSumado}`); */
   dicePng.src = `dice-${luckyNumber}.png`;
   console.log(luckyNumber);
-  if (luckyNumber === 1) {
-    activePlayer.children.textContent = '0';
-    freezePlayer;
-  }
+  /* if (luckyNumber !== 1) {
+    for (i = 1; i > luckyNumber; i++) {
+      let x = 
+    }
+  } */
   return luckyNumber;
 };
 
@@ -32,14 +42,26 @@ const resetGame = () => {
     score[i].textContent = '0';
     currentScore[i].textContent = '0';
   }
+  dicePng.classList.add('hidden');
   //document.querySelector('#score--1').textContent = '0';
   //document.querySelector('#score--0').textContent = '0';
 };
 
 const freezePlayer = function () {
-  active;
+  let w = Number(activeScore.textContent);
+  console.log(w);
+  scorePlayer1.textContent = `${w}`;
 };
 
 btnNewGame.addEventListener('click', resetGame);
 btnRollDice.addEventListener('click', randomNumber);
 btnHold.addEventListener('click', freezePlayer);
+
+/* Mientras luckyNumber no sea igual a 1, continuar a sumar los numeros
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+while (diceRoll !== 6) {
+  console.log(`This is not 6, this is: ${diceRoll}`);
+  diceRoll = Math.floor(Math.random() * 6) + 1;
+  if (diceRoll === 6) console.log(`This is a ${diceRoll}`);
+} */
